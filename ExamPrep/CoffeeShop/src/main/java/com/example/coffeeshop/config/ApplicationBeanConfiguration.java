c980f8b4e1574936a89e60de38813ac5427786d7
nanko89 <natali.v.angelova@gmail.com>
2022-05-28 21:54:30 +0300

    Update coffee shop

--- ExamPrep/CoffeeShop/src/main/java/com/example/coffeeshop/config/ApplicationBeanConfiguration.java
+++ ExamPrep/CoffeeShop/src/main/java/com/example/coffeeshop/config/ApplicationBeanConfiguration.java
@@ -3,7 +3,7 @@ package com.example.coffeeshop.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+
 
 @Configuration
 public class ApplicationBeanConfiguration {
