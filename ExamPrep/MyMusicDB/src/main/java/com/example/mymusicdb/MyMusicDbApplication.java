5a22bf3453c51519187994a22e0e08038ded8404
Natali Angelova <natali.v.angelova@gmail.com>
2022-06-14 13:48:19 +0300

    My Music resources

---
+++ ExamPrep/MyMusicDB/src/main/java/com/example/mymusicdb/MyMusicDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymusicdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMusicDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMusicDbApplication.class, args);
+    }
+
+}
---
+++ ExamPrep/MyMusicDB/src/test/java/com/example/mymusicdb/MyMusicDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymusicdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMusicDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
