cdd3d974193b113c6a0f80a115b1cf756a6c4ae6
Natali Angelova <natali.v.angelova@gmail.com>
2022-06-22 19:39:17 +0300

    Update file

---
+++ ExamPrep/Andrey/src/main/java/com/example/andrey/model/validation/UniqueEmailValidator.java
@@ -0,0 +1,23 @@
+package com.example.andrey.model.validation;
+
+
+import com.example.andrey.service.UserService;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+    private final UserService userService;
+
+    public UniqueEmailValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        if (email == null){
+            return true;
+        }
+        return userService.isFreeEmail(email);
+    }
+}
